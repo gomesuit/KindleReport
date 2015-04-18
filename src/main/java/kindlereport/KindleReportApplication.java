1a04bafbf908c0795c83bd20de69e5df21dd8237
gomesuit <gomesuit@gmail.com>
2015-04-18 20:21:03 +0900

    test
    test

--- src/main/java/kindlereport/KindleReportApplication.java
+++ src/main/java/kindlereport/KindleReportApplication.java
@@ -1,23 +1,10 @@
 package kindlereport;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@EnableAutoConfiguration
+@SpringBootApplication
 public class KindleReportApplication {
-	
-	@RequestMapping("/")
-	public String home() {
-		return "index";
-	}
 	
     public static void main(String[] args) {
         SpringApplication.run(KindleReportApplication.class, args);
