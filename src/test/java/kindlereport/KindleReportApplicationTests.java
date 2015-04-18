647f7ab92680202f0160ceecbf0b54716f84077d
gomesuit <gomesuit@gmail.com>
2015-04-18 20:12:24 +0900

    first commit
    first commit

---
+++ src/test/java/kindlereport/KindleReportApplicationTests.java
@@ -0,0 +1,16 @@
+package kindlereport;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = KindleReportApplication.class)
+public class KindleReportApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
