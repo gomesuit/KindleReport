c081636dd1f7690764aeead501a0310f1e5d3b9b
gomesuit <gomesuit@gmail.com>
2015-05-06 02:25:45 +0900

    websocket改善
    websocket改善

--- src/main/resources/static/js/websocket.js
+++ src/main/resources/static/js/websocket.js
@@ -1,18 +1,43 @@
 $(function() {
-    var ws = new WebSocket("ws://localhost/echo");
+	var url = "ws://" + location.host + "/echo";
+    var ws = new WebSocket(url);
     ws.onopen = function(){
     };
     ws.onclose = function(){
     };
-    ws.onmessage = function(message){
-        $("#result").append(message.data).append("<br>");
+    ws.onmessage = function(json){
+    	var data = $.parseJSON(json.data);
+        noticeBalloon(data);
+        $("#result").append(makeComment(data.message));
     };
+	function noticeBalloon(data) {
+	    $.amaran({
+	        'theme'     :'user blue',
+	        'content'   :{
+	            img:data.imgUrl,
+	            user:data.title,
+	            message:data.message
+	        },
+	        'position'  :'bottom right'
+	    });
+	}
+	function makeComment(str) {
+		return '<p class="detailComment">' + str + '</p>'
+	}
     ws.onerror = function(event){
         alert("接続に失敗しました。");
     };
 	$(document).on("submit", "#form", function(){
-        ws.send($("#message").val());
-        $("#message").val("")
+		var message = $("#message").val();
+		var imgUrl = $("img").attr("src");
+		var title = $("h1").text();
+		var data = JSON.stringify({
+			message: message,
+			imgUrl: imgUrl,
+			title: title,
+		});
+        ws.send(data);
+        $("#message").val("");
         return false;
     });
 });
