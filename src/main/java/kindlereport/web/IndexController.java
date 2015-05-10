347301a913403ca2ba963d12daefa64d71b0063b
gomesuit <gomesuit@gmail.com>
2015-05-11 00:35:36 +0900

    ファイル整理
    ファイル整理

--- src/main/java/kindlereport/web/IndexController.java
+++ src/main/java/kindlereport/web/IndexController.java
@@ -6,7 +6,6 @@ import java.util.List;
 import kindlereport.dao.KindleMapper;
 import kindlereport.model.DateKindleList;
 import kindlereport.model.Kindle;
-import kindlereport.service.MyBatisService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -21,7 +20,6 @@ public class IndexController {
 	
 	@Autowired
 	private KindleMapper kindleMapper;
-	private MyBatisService myBatisService = new MyBatisService();
 	
 	@RequestMapping(LIST_PAGE_URL)
 	public String ajax(Model model) {
@@ -29,33 +27,6 @@ public class IndexController {
 		model.addAttribute("DATELIST_PAGE_URL", DATELIST_PAGE_URL);
 		
 		return LIST_PAGE_URL;
-	}
-	
-	@RequestMapping("dayList")
-	public String dayList(Model model) {
-		int colnum = 4;
-		
-		List<DateKindleList> dateKindleListList = new ArrayList<DateKindleList>();
-		
-		List<String> dateList = kindleMapper.selectRereaseDateList();
-		for(String date : dateList){
-			DateKindleList dateKindleList = new DateKindleList();
-			dateKindleList.setReleaseDate(date);
-			dateKindleList.setRowKindleList(new ArrayList<List<Kindle>>());
-			List<Kindle> KindleList = kindleMapper.selectDayKindleList(date);
-			for(int i = 0; KindleList.size() > i; i = i + colnum){
-				int start = i;
-				int end = i + colnum;
-				if(KindleList.size() < end){
-					end = KindleList.size();
-				}
-				dateKindleList.getRowKindleList().add(KindleList.subList(start, end));
-			}
-			dateKindleListList.add(dateKindleList);
-		}
-		
-		model.addAttribute("dateKindleListList", dateKindleListList);
-		return "dateKindleList";
 	}
 
 	@RequestMapping(DATELIST_PAGE_URL)
