8792b68e366bf7f436012aa014a0b0837005dacc
gomesuit <gomesuit@gmail.com>
2015-05-16 21:31:17 +0900

    mysql化

--- src/main/java/kindlereport/dao/KindleMapper.java
+++ src/main/java/kindlereport/dao/KindleMapper.java
@@ -6,25 +6,9 @@ import java.util.List;
 import java.util.Map;
 
 public interface KindleMapper {
-  int countKindle(String asin);
-  List<String> selectSortValue(String searchIndex);
-  List<String> selectBrowseNodes(String searchIndex);
-  List<String> selectPowerPubdates(String searchIndex);
-  int insertRequestLog(Map<String,String> map);
-  void insertKindleRegist(String asin);
-  void updateKindleRegist(String asin);
   List<Kindle> selectTodayKindleList(String date);
-  Map<String,String> selectKindleShinchaku(List<String> wordList);
   Map<String,String> selectKindle(String asin);
-  void updateTweetShinchaku(String asin);
-  Map<String,String> selectKindleTodaySales(List<String> wordList);
-  void updateTweetTodaySales(String asin);
-  Map<String,String> selectMinTweetTop3();
-  void countupTweetTop3(String description);
   List<String> selectExclusion();
-  void insertNoImage(String asin);
-  String selectNoImage();
-  void deleteNoImage(String asin);
   List<Kindle> selectKindleList(Map<String,Integer> map);
   List<String> selectRereaseDateList(String date);
   List<Kindle> selectDayKindleList(String releaseDate);
