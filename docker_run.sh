b61dff513b7a48ba45b462aeba06b3857d928aa2
gomesuit <gomesuit@gmail.com>
2016-04-07 00:59:14 +0900

    Refactoring

--- docker_run.sh
+++ docker_run.sh
@@ -2,4 +2,10 @@
 
 docker stop kindlereport
 docker rm kindlereport
-docker run -d --name kindlereport -e JAVA_OPTS="-Xms192m -Xmx192m -XX:MaxPermSize=64m" -e LANG=en_US.UTF-8 -p 8080:8080 --link mysql:mysql-server kindlereport
+docker run -d \
+	--name kindlereport \
+	-e JAVA_OPTS="-Xms192m -Xmx192m -XX:MaxPermSize=64m" \
+	-e LANG=en_US.UTF-8 \
+	-p 8080:8080 \
+	--link mysql:mysql-server \
+	kindlereport
