c76f2e5d7610e135dfd376ff3ab3f3c83b67b2d2
gomesuit <gomesuit@gmail.com>
2016-04-23 16:43:13 +0900

    gix top

--- src/main/java/kindlereport/web/IndexController.java
+++ src/main/java/kindlereport/web/IndexController.java
@@ -4,9 +4,14 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import kindlereport.model.KindleTile;
 import kindlereport.service.KindleService;
+import kindlereport.web.model.DateKindle;
+import kindlereport.web.model.OrderType;
 import kindlereport.web.util.DateUtil;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,12 +20,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
+	private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
 	
 	@Autowired
 	private KindleService kindleService;
 
 	@RequestMapping("/")
 	public String home(Model model, HttpServletRequest request) {
+		
+		List<KindleTile> newRegistList = kindleService.getKindleList(5, 0, OrderType.insertTimeDesc, null, false);
+		logger.debug("newRegistList = {}", newRegistList);
+		model.addAttribute("newRegistList", newRegistList);
+		
+		DateKindle dateKindle = kindleService.getDateKindleList(DateUtil.getTodayByString()).get(0);
+		List<KindleTile> todayReleaseList = dateKindle.getKindleList();
+		logger.debug("todayReleaseList = {}", todayReleaseList);
+		model.addAttribute("todayReleaseList", todayReleaseList);
 		
 		request.setAttribute("pageName", "front");
 		return "common_frame";
